3ca7fb0b77d5af2dd772c81c3b1ba047bd940d9c
Josef Ilg <josef.ilg@fau.de>
2019-11-15 14:57:28 +0200

    clean up

--- directory_utils.py
+++ directory_utils.py
@@ -1,33 +1,33 @@
-import os, stat, types
+import os
 
 
-def makeHTMLtable(top, depthfirst=False):
-    from xml.sax.saxutils import escape  # To quote out things like &amp;
-    ret = ['<table class="fileList">\n']
-    for top, names in walktree(top):
-        ret.append('   <tr><td class="directory">%s</td></tr>\n' % escape(top))
-        for name in names:
-            ret.append('   <tr><td class="file">%s</td></tr>\n' % escape(name))
-    ret.append('</table>')
-    return ''.join(ret)  # Much faster than += method
-
-
-def walktree(top=".", depthfirst=True):
-    """Walk the directory tree, starting from top. Credit to Noah Spurrier and Doug Fort."""
-
-    names = os.listdir(top)
-    if not depthfirst:
-        yield top, names
-    for name in names:
-        try:
-            st = os.lstat(os.path.join(top, name))
-        except os.error:
-            continue
-        if stat.S_ISDIR(st.st_mode):
-            for (newtop, children) in walktree(os.path.join(top, name), depthfirst):
-                yield newtop, children
-    if depthfirst:
-        yield top, names
+# def makeHTMLtable(top, depthfirst=False):
+#     from xml.sax.saxutils import escape  # To quote out things like &amp;
+#     ret = ['<table class="fileList">\n']
+#     for top, names in walktree(top):
+#         ret.append('   <tr><td class="directory">%s</td></tr>\n' % escape(top))
+#         for name in names:
+#             ret.append('   <tr><td class="file">%s</td></tr>\n' % escape(name))
+#     ret.append('</table>')
+#     return ''.join(ret)  # Much faster than += method
+#
+#
+# def walktree(top=".", depthfirst=True):
+#     """Walk the directory tree, starting from top. Credit to Noah Spurrier and Doug Fort."""
+#
+#     names = os.listdir(top)
+#     if not depthfirst:
+#         yield top, names
+#     for name in names:
+#         try:
+#             st = os.lstat(os.path.join(top, name))
+#         except os.error:
+#             continue
+#         if stat.S_ISDIR(st.st_mode):
+#             for (newtop, children) in walktree(os.path.join(top, name), depthfirst):
+#                 yield newtop, children
+#     if depthfirst:
+#         yield top, names
 
 
 def starting(top):
--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import socket
-from os import listdir, os
-from os.path import isfile, join
-
 import http_utils as hu
-from directory_utils import makeHTMLtable, starting
+from directory_utils import starting
 
 HOST, PORT = 'localhost', 8888
 
@@ -15,35 +12,22 @@ listen_socket.listen(1)
 while True:
     client_connection, client_address = listen_socket.accept()
     request_data = hu.decode_http(client_connection.recv(1024))
-    parent_path = "."
     try:
-        path = parent_path + request_data["Request"].split()[1]
+        path = "." + request_data["Request"].split()[1]
         print(request_data["Request"].split()[1])
         string = starting(path)
     except:
-        #TODO Exception
+        # TODO Exception
         string = ""
-        #print("___Exception!___")
-
-    if os.path.isfile(path):
-        client_connection..send("EXISTS " + str(os.path.getsize(filename)))
-        userResponse = sock.recv(1024)
-        if userResponse[:2] == 'OK':
-            with open(filename, 'rb') as f:
-                bytesToSend = f.read(1024)
-                sock.send(bytesToSend)
-                while bytesToSend != "":
-                    bytesToSend = f.read(1024)
-                    sock.send(bytesToSend)
-    else:
-        sock.send("ERR ")
+        # print("___Exception!___")
 
     html = "".join(['<html>',
                     '<head>',
                     '   <title>Search results</title>',
                     '</head>',
                     '<body>',
-                    "<a href="+ parent_path+">Go back</a><br/><br/>\n",
+                    # TODO Back button doesn't work
+                    "<a href='..'>Go back</a><br/><br/>",
                     string,
                     '</body>',
                     '</html>'])
